aa754d713ad2674669a5b6dda31c1e373d778979
owolabi-develop <owolabidevelop84@gmail.com>
2022-03-28 07:43:09 -0700

    change kinesisstreamStack name to covid19_death_rate

---
+++ kinesis_stream/kinesis_stream_stack.py
@@ -0,0 +1,16 @@
+from constructs import Construct
+from aws_cdk import(
+    Stack,
+    aws_kinesis as kinesis,
+    Duration
+    )
+
+class KinesisStreamStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+        covid19_death_rate = kinesis.Stream(self,
+                                            "Covid19DeathRate",
+                                            stream_name="DeathRate",
+                                            retention_period=Duration.hours(24),
+                                            shard_count=1
+                                            )
